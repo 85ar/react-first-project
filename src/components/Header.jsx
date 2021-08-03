a172a3a975bae005703ced519bbe44f9cf4ee407
85ar <85ar@rambler.ru>
2021-08-03 10:37:54 +0500

    fix: components1

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+// import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://ensocore.com/media/61/reactjs-logo-sticker%20%281%29.jpg"
+        alt=""
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://cdn.sandals.com/beaches/v12/images/general/destinations/home/beach.jpg"
+          alt=""
+        />
+      </div>
+      <div>ava + descr</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
